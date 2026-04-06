45d161c3e409b50b4e083ed9731e334d91fda33b
Feroz <mohamedferoz2023@gmail.com>
2026-04-06 18:11:23 +0530

    footer is placed at bottom

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,4 +1,4 @@
-import {  Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import LandingPage from "./pages/LandingPage";
@@ -11,25 +11,24 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import TermsCondition from "./pages/TermsCondition";
 
-
 function App() {
- 
-
   return (
-    <>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/register" element={<SignIn />} />
-      <Route path="/analysis" element={<Analysis />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/resume" element={<Resume />} />
-      <Route path="/view-resume" element={<ViewResume />} />
-      <Route path="/terms" element={<TermsCondition />} />
-    </Routes>
-    <Footer/>
-    </>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/register" element={<SignIn />} />
+          <Route path="/analysis" element={<Analysis />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/view-resume" element={<ViewResume />} />
+          <Route path="/terms" element={<TermsCondition />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
